69631284c90a92b1447b9a7b45c9fe7d53a00718
Lục Thế Vỹ <23110368@student.hcmute.edu.vn>
2026-06-08 04:41:16 +0700

    remove unused

--- app/src/main/java/com/example/maxlish/MainActivity.kt
+++ app/src/main/java/com/example/maxlish/MainActivity.kt
@@ -7,7 +7,6 @@ import com.example.maxlish.data.helper.NotificationHelper
 import com.example.maxlish.data.helper.NotificationScheduler
 import com.example.maxlish.data.repository.FirebaseAuthRepository
 import com.example.maxlish.ui.navigation.AppNavGraph
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
 
